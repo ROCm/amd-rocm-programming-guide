7d9da6920e53217b0e973b034f48006b0601051d
Istvan Kiss <neon60@gmail.com>
2025-12-15 12:20:59 +0100

    Update to docs/7.1.1 branch

--- docs/tools/update_hip_doc_files.py
+++ docs/tools/update_hip_doc_files.py
@@ -1,9 +1,7 @@
 import urllib.request
 
-repo = "ROCm/hip"
-branch = "docs/develop"
-# repo = "ROCm/rocm-systems"
-# branch = "develop/projects/hip"
+repo = "ROCm/rocm-systems"
+branch = "docs/7.1.1"
 
 # "https://raw.githubusercontent.com/ROCm/rocm-systems/refs/heads/develop/projects/hip/docs/how-to/hip_runtime_api.rst",
 # https://raw.githubusercontent.com/ROCm/rocm-systems/refs/heads/release/rocm-rel-7.0/projects/hip/docs/how-to/hip_runtime_api.rst
@@ -11,113 +9,113 @@ branch = "docs/develop"
 # Update update_example_codes.py
 
 urllib.request.urlretrieve(
-     f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/docs/tools/update_example_codes.py",
+     f"https://raw.githubusercontent.com/ROCm/rocm-systems/refs/heads/{branch}/projects/hip/docs/tools/update_example_codes.py",
      "docs/tools/update_example_codes.py"
 )
 
 # hip_runtime.rst
 
 urllib.request.urlretrieve(
-     f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/docs/how-to/hip_runtime_api.rst",
+     f"https://raw.githubusercontent.com/ROCm/rocm-systems/refs/heads/{branch}/projects/hip/docs/how-to/hip_runtime_api.rst",
      "docs/how-to/hip_runtime_api.rst"
 )
 
 # hip_runtime
 
 urllib.request.urlretrieve(
-     f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/docs/how-to/hip_runtime_api/asynchronous.rst",
+     f"https://raw.githubusercontent.com/ROCm/rocm-systems/refs/heads/{branch}/projects/hip/docs/how-to/hip_runtime_api/asynchronous.rst",
      "docs/how-to/hip_runtime_api/asynchronous.rst"
 )
 
 urllib.request.urlretrieve(
-     f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/docs/how-to/hip_runtime_api/call_stack.rst",
+     f"https://raw.githubusercontent.com/ROCm/rocm-systems/refs/heads/{branch}/projects/hip/docs/how-to/hip_runtime_api/call_stack.rst",
      "docs/how-to/hip_runtime_api/call_stack.rst"
 )
 
 urllib.request.urlretrieve(
-     f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/docs/how-to/hip_runtime_api/cooperative_groups.rst",
+     f"https://raw.githubusercontent.com/ROCm/rocm-systems/refs/heads/{branch}/projects/hip/docs/how-to/hip_runtime_api/cooperative_groups.rst",
      "docs/how-to/hip_runtime_api/cooperative_groups.rst"
 )
 
 urllib.request.urlretrieve(
-     f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/docs/how-to/hip_runtime_api/error_handling.rst",
+     f"https://raw.githubusercontent.com/ROCm/rocm-systems/refs/heads/{branch}/projects/hip/docs/how-to/hip_runtime_api/error_handling.rst",
      "docs/how-to/hip_runtime_api/error_handling.rst"
 )
 
 urllib.request.urlretrieve(
-     f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/docs/how-to/hip_runtime_api/external_interop.rst",
+     f"https://raw.githubusercontent.com/ROCm/rocm-systems/refs/heads/{branch}/projects/hip/docs/how-to/hip_runtime_api/external_interop.rst",
      "docs/how-to/hip_runtime_api/external_interop.rst"
 )
 
 urllib.request.urlretrieve(
-     f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/docs/how-to/hip_runtime_api/hipgraph.rst",
+     f"https://raw.githubusercontent.com/ROCm/rocm-systems/refs/heads/{branch}/projects/hip/docs/how-to/hip_runtime_api/hipgraph.rst",
      "docs/how-to/hip_runtime_api/hipgraph.rst"
 )
 
 urllib.request.urlretrieve(
-     f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/docs/how-to/hip_runtime_api/initialization.rst",
+     f"https://raw.githubusercontent.com/ROCm/rocm-systems/refs/heads/{branch}/projects/hip/docs/how-to/hip_runtime_api/initialization.rst",
      "docs/how-to/hip_runtime_api/initialization.rst"
 )
 
 urllib.request.urlretrieve(
-     f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/docs/how-to/hip_runtime_api/memory_management.rst",
+     f"https://raw.githubusercontent.com/ROCm/rocm-systems/refs/heads/{branch}/projects/hip/docs/how-to/hip_runtime_api/memory_management.rst",
      "docs/how-to/hip_runtime_api/memory_management.rst"
 )
 
 # The multi device downloaded to a different location
 urllib.request.urlretrieve(
-     f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/docs/how-to/hip_runtime_api/multi_device.rst",
+     f"https://raw.githubusercontent.com/ROCm/rocm-systems/refs/heads/{branch}/projects/hip/docs/how-to/hip_runtime_api/multi_device.rst",
      "docs/how-to/multi-gpu/multi_device.rst"
 )
 
 urllib.request.urlretrieve(
-     f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/docs/how-to/hip_runtime_api/opengl_interop.rst",
+     f"https://raw.githubusercontent.com/ROCm/rocm-systems/refs/heads/{branch}/projects/hip/docs/how-to/hip_runtime_api/opengl_interop.rst",
      "docs/how-to/hip_runtime_api/opengl_interop.rst"
 )
 
 # hip_runtime / memory_management
 
 urllib.request.urlretrieve(
-     f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/docs/how-to/hip_runtime_api/memory_management/virtual_memory.rst",
+     f"https://raw.githubusercontent.com/ROCm/rocm-systems/refs/heads/{branch}/projects/hip/docs/how-to/hip_runtime_api/memory_management/virtual_memory.rst",
      "docs/how-to/hip_runtime_api/memory_management/virtual_memory.rst"
 )
 
 urllib.request.urlretrieve(
-     f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/docs/how-to/hip_runtime_api/memory_management/coherence_control.rst",
+     f"https://raw.githubusercontent.com/ROCm/rocm-systems/refs/heads/{branch}/projects/hip/docs/how-to/hip_runtime_api/memory_management/coherence_control.rst",
      "docs/how-to/hip_runtime_api/memory_management/coherence_control.rst"
 )
 
 urllib.request.urlretrieve(
-     f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/docs/how-to/hip_runtime_api/memory_management/device_memory.rst",
+     f"https://raw.githubusercontent.com/ROCm/rocm-systems/refs/heads/{branch}/projects/hip/docs/how-to/hip_runtime_api/memory_management/device_memory.rst",
      "docs/how-to/hip_runtime_api/memory_management/device_memory.rst"
 )
 
 urllib.request.urlretrieve(
-     f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/docs/how-to/hip_runtime_api/memory_management/host_memory.rst",
+     f"https://raw.githubusercontent.com/ROCm/rocm-systems/refs/heads/{branch}/projects/hip/docs/how-to/hip_runtime_api/memory_management/host_memory.rst",
      "docs/how-to/hip_runtime_api/memory_management/host_memory.rst"
 )
 
 urllib.request.urlretrieve(
-     f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/docs/how-to/hip_runtime_api/memory_management/stream_ordered_allocator.rst",
+     f"https://raw.githubusercontent.com/ROCm/rocm-systems/refs/heads/{branch}/projects/hip/docs/how-to/hip_runtime_api/memory_management/stream_ordered_allocator.rst",
      "docs/how-to/hip_runtime_api/memory_management/stream_ordered_allocator.rst"
 )
 
 urllib.request.urlretrieve(
-     f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/docs/how-to/hip_runtime_api/memory_management/unified_memory.rst",
+     f"https://raw.githubusercontent.com/ROCm/rocm-systems/refs/heads/{branch}/projects/hip/docs/how-to/hip_runtime_api/memory_management/unified_memory.rst",
      "docs/how-to/hip_runtime_api/memory_management/unified_memory.rst"
 )
 
 # hip_runtime / memory_management / device_memory
 
 urllib.request.urlretrieve(
-     f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/docs/how-to/hip_runtime_api/memory_management/device_memory/texture_fetching.rst",
+     f"https://raw.githubusercontent.com/ROCm/rocm-systems/refs/heads/{branch}/projects/hip/docs/how-to/hip_runtime_api/memory_management/device_memory/texture_fetching.rst",
      "docs/how-to/hip_runtime_api/memory_management/device_memory/texture_fetching.rst"
 )
 
 # tutorial
 
 urllib.request.urlretrieve(
-     f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/docs/tutorial/reduction.rst",
+     f"https://raw.githubusercontent.com/ROCm/rocm-systems/refs/heads/{branch}/projects/hip/docs/tutorial/reduction.rst",
      "docs/tutorial/reduction.rst"
 )
 
